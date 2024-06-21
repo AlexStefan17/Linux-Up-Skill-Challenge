bab844f235da1983d332b95baca842c292f87483
Alex Stefan <stefan.alex170299@gmail.com>
2024-06-21 15:41:07 +0300

    day14

---
+++ Day14/day14.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+cd /home/
+ls -lh
+umask -S
+umask
+sudo chgrp canac nancy
+sudo su canac
+cd nancy/
+touch dnd-session
+sudo chmod g+w nancy
+sudo su canac
+touch txt.txt
+sudo chown canac nancy
+
+sudo chmod 754 nancy
+sudo chown root nancy
+sudo chown :root nancy
